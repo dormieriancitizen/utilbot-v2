56941b7be3e33f322164afcb99a464ef71f853bf
UrMum <dormieriancitizen@gmail.com>
2024-10-05 11:14:42 -0400

    requirements

--- main.py
+++ main.py
@@ -21,13 +21,74 @@ async def icon(ctx, member: discord.Member):
     await ctx.reply(member.avatar)
 
 @bot.command()
+async def nuke(ctx, count: int):
+    messages = [message async for message in ctx.channel.history(limit=count)]
+    for message in messages:
+        await message.delete()
+
+@bot.command()
+async def status(ctx, *args):
+  activity = discord.Game(name=' '.join(args))
+  await bot.change_presence(status=discord.Status.online, activity=activity)
+  await ctx.message.delete()
+
+@bot.command()
 async def count(ctx,searchtype,*args):
+    m = await ctx.reply("Loading...")
+
     search_string = " ".join(args)
     if searchtype=="count":
         message_count = await search.get_messages_count(ctx.guild, search_string)
+        await ctx.reply(f"{message_count} messages")
+        return
     elif searchtype=="user":
-        for user in ctx.guild.members:
-            print(user)
-    await ctx.reply(str(message_count))
+        counts = {}
+        for member in (await ctx.guild.fetch_members()):
+            member_count = await search.get_messages_count(ctx.guild, search_string, 
+                                                           args=search.generate_search_arguments(author_id=str(member.id))
+                                                           )
+            counts[member.name] = member_count
+        
+        response = f"# {search_string if search_string else 'Users'}\n"
+        response += "\n".join([f"-`{member}` has sent `{counts[member]}` messages" for member in counts])
+        print(response)
+        await m.edit(response)
+        return
+    elif searchtype=="channel":
+        counts = {}
+        for channel in ctx.guild.text_channels:
+            channel_count = await search.get_messages_count(ctx.guild, search_string, 
+                                                           args=search.generate_search_arguments(channel_id=str(channel.id))
+                                                           )
+            counts[channel.name] = channel_count
+        response = f"# {search_string if search_string else 'Channels'}\n"
+        response += "\n".join([f"-`{channel}` has {counts[channel]} messages" for channel in counts])
+        print(response)
+        await m.edit(response)
+        return
+    else:
+        await m.edit(f"ERR: Searchtype {searchtype} does not exist.")
+
+
+async def _spam(channel,string,count):
+    if len(string) > 2000:
+        raise Exception("String must be shorter than 2000 characters")
+
+    for i in range(count):
+        await channel.send(string)
+
+@bot.command()
+async def spam(ctx,count: int,*args):
+    await ctx.message.delete()
+    await _spam(ctx.channel," ".join(args), count)
+
+@bot.command()
+async def lag(ctx,count: int,channel=None):
+    if channel is None:
+        channel = ctx.channel
+    else:
+        channel = await discord.get_channel(channel)
+
+    await _spam(channel,398*"🅰",count)
 
 bot.run(os.getenv("TOKEN"))
--- search.py
+++ search.py
@@ -1,16 +1,16 @@
-import urllib.parse, math, requests, asyncio, os
+import math, requests, asyncio, os
 from dotenv import load_dotenv
 load_dotenv()
 
 async def individual_search(guild,args):
     while True:
         url = f"https://discord.com/api/v9/guilds/{guild.id}/messages/search?{args}"
-        print(url)
         result = requests.get(url,headers={"authorization":os.getenv("TOKEN")})
 
         if result.status_code == 429:
             print(f"Ratelimited for {result.json()['retry_after']}, retrying")
-            await asyncio.sleep(int(result.json()["retry_after"]))
+            # Discord pretty consistently waits an extra 0.3s
+            await asyncio.sleep(int(result.json()["retry_after"])+0.35)
         else:
             return result.json()
 
@@ -35,14 +35,13 @@ def generate_search_arguments(content=None,author_id=None,channel_id=None,has=No
     if author_id:
         args += "author_id="+author_id+"&"
     if channel_id:
-        args += "channel_id"+channel_id+"&"
+        args += "channel_id="+channel_id+"&"
     if has:
         args += "has"+has+"&"
-    
+    # print(args)
     return args
 
 async def get_messages_count(guild,search_string,args=""):
     args += generate_search_arguments(content=search_string,limit=1)
     response = (await individual_search(guild,args))
-    print(response)
     return int(response["total_results"])
